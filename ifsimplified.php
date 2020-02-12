89884f43d27f891e8aa50849d1d9d72add612bb7
tallyvanv <talisa.vdv@gmail.com>
2020-02-12 18:04:19 +0100

    simplified if finished

---
+++ ifsimplified.php
@@ -0,0 +1,21 @@
+<?php
+/*if (isset($_POST['user'])) {
+    echo $_POST['user'];
+}
+else {
+    echo "ain't nothing here";
+}*/
+
+$username = $_POST['user'] ?? $_POST['user'] ?? "ain't nothing ere bruv";
+
+echo $username;
+
+$username = $_POST['user'] ?? $_POST['user'] ?? "ain't nothing ere bruv";
+
+echo $username;
+?>
+
+<form method="post">
+    Username:<input type="text" id="user" name="user" class="form-control" value=""/>
+    <button type="submit" class="btn btn-primary">Test!</button>
+</form>
